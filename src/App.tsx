df97b1c105aadd32ad0c3b60e6001e7b6b753db7
Quảng Phú Hưng <quangphuhung2k6@gmail.com>
2026-03-19 21:52:31 +0700

    Update main screen: adjust layout, colors, fonts, order, and typography based on Figma design :P

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 function App() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-primary-500">URL Shortener</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-primary-500">Devcamp URL Shortener</h1>
+      <p className="text-lg text-primary-500">
+        Simplify, Organize, and Share: URL Management Made Easy
+      </p>
+      <form>
+        <label className="font-semibold text-primary-500">Your long URL</label>
+        <div className="flex gap-38">
+          <input type="text" />
+          <button className="bg-primary-500 text-white font-bold px-6 py-2 rounded-md">
+            Shorten
+          </button>
+        </div>
+      </form>
     </main>
   )
 }
