5931f8a5dec0cfe43badcfd1025f9df189b9fbc1
Hoang Quoc Vinh <vinh.hoangvinhhochay@hcmut.edu.vn>
2026-03-23 22:52:07 +0700

    update logic shorten link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [longUrl, setLongUrl] = useState('') // 1. Nhớ cái link dài người dùng gõ
+  const [shortUrl, setShortUrl] = useState('') // 2. Nhớ cái link ngắn sau khi rút gọn
+  const [isModalOpen, setIsModalOpen] = useState(false) // 3. Nhớ xem bảng kết quả đang đóng (false) hay mở (true)
+  const handleShorten = () => {
+    if (longUrl === '') {
+      alert('Please paste your link here!')
+      return
+    }
+    setShortUrl('https://shortenlink.123')
+    setIsModalOpen(true)
+    console.log('link shorten:', longUrl)
+  }
+  const handleCopy = () => {
+    navigator.clipboard.writeText(shortUrl)
+    alert('Copied')
+  }
+
   return (
     <main className="app-container">
       <nav className="top-nav">
@@ -33,11 +51,35 @@ function App() {
               type="text"
               className="url-input"
               placeholder="Input the URL you want to shorten"
+              value={longUrl}
+              onChange={e => setLongUrl(e.target.value)}
             />
           </div>
-          <button className="submit-btn">Shorten</button>
+          <button className="submit-btn" onClick={handleShorten}>
+            Shorten{' '}
+          </button>
         </div>
       </div>
+
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-box">
+            <h2 className="modal-title">Link shortened!</h2>
+
+            <div className="result-container">
+              {}
+              <input type="text" className="result-input" value={shortUrl} readOnly />
+              <button className="copy-btn" onClick={handleCopy}>
+                Copy
+              </button>
+            </div>
+
+            <button className="close-btn" onClick={() => setIsModalOpen(false)}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </main>
   )
 }
