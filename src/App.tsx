6ae2d6a23b38561280d6091fb93b6b11c7e23f5a
tlminhhoang-attt-uit <tranleminhhoang2007@gmail.com>
2026-03-23 20:10:58 +0700

    feat: complete assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
+import { useState } from 'react'
+import './App.css'
 function App() {
+  const [inp, set] = useState('')
+  const [res, scr] = useState('')
+  const [cop, setcop] = useState(false)
+
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-primary-500">URL Shortener</h1>
-    </main>
+    <>
+      {cop && <div className="not">The link has been copied to the clipboard</div>}
+      <main className="con">
+        <h1 className="tit">URL Shortener</h1>
+        <input
+          type="text"
+          placeholder="Please enter your URL ..."
+          value={inp}
+          onChange={eve => set(eve.target.value)}
+          className="input"
+        />
+        <button
+          className="but"
+          onClick={() => {
+            if (!inp) {
+              return
+            }
+            const yea: number[] = []
+            for (let a = 1930; a <= 2026; a += 4) {
+              yea.push(a)
+            }
+            const ind = Math.floor(Math.random() * yea.length)
+            const num = yea[ind]
+            const lin = 'https://fifa.wc/' + num
+            scr(lin)
+            setcop(false)
+          }}
+        >
+          Make it shorter
+        </button>
+        {res && (
+          <div className="fra">
+            <p className="ans">{res}</p>
+            <button
+              className="but2"
+              onClick={() => {
+                navigator.clipboard.writeText(res)
+                setcop(true)
+                setTimeout(() => {
+                  setcop(false)
+                }, 2000)
+              }}
+            >
+              Copy
+            </button>
+          </div>
+        )}
+      </main>
+    </>
   )
 }
-
 export default App
