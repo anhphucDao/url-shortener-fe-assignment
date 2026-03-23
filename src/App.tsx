e0450dadcb500052692893458df18221695f7c4c
OtisHuang-new <khoihoang862006@gmail.com>
2026-03-23 17:49:22 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -25,18 +25,22 @@ function App() {
     <>
       <Header />
       <main className="min-h-screen flex flex-col items-center justify-center font-sans">
-        <p className="font-sans text-[60px] font-bold text-primary-500">Devcamp URL Shortener</p>
-        <p className="font-sans text-[28px] text-primary-500 mb-4">
+        <p className="font-sans lg:text-[60px] sm:text-[40px] text-[35px] font-bold text-primary-500">
+          Devcamp URL Shortener
+        </p>
+        <p className="font-sans lg:text-[28px] sm:text-[18px] text-[15px] text-primary-500 mb-4">
           Simplify, Organize, and Share: URL Management Made Easy
         </p>
         {/* Input phase */}
-        <div className="w-208 h-38 shadow-[0px_0px_50px_rgba(0,0,0,0.25)] rounded-[10px] flex items-center justify-center py-2">
-          <div className=" w-186">
-            <p className="font-semibold text-[20px] text-primary-500">Your long URL</p>
-            <div className="flex flex-row py-2">
-              <div className="flex flex-row items-center px-1  w-152 border rounded-[10px] border-gray-400 divide-x divide-gray-400">
+        <div className="w-[50%] min-w-100 lg:h-38 h-26 shadow-[0px_0px_50px_rgba(0,0,0,0.25)] rounded-[10px] flex items-center justify-center">
+          <div className=" w-[90%]">
+            <p className="font-semibold lg:text-[20px] sm:text-[17px] text-[14px] text-primary-500">
+              Your long URL
+            </p>
+            <div className="flex flex-row py-2 gap-2">
+              <div className="flex flex-row items-center px-1 w-[80%] border rounded-[10px] border-gray-400 divide-x divide-gray-400">
                 <Link2
-                  className="w-12 flex-none mr-1 my-2 hover:cursor-pointer"
+                  className="sm:w-12 w-8 flex-none mr-1 my-2 hover:cursor-pointer"
                   onClick={async () => {
                     try {
                       setInput_url(await navigator.clipboard.readText())
@@ -46,14 +50,14 @@ function App() {
                   }}
                 />
                 <input
-                  className="w-1 flex-1 mx-1 my-2 focus:outline-none"
+                  className="w-full mx-1 my-2 focus:outline-none"
                   value={input_url}
                   onChange={e => setInput_url(e.target.value)}
                   placeholder="Input the URL you want to shorten"
                 />
               </div>
               <button
-                className="w-24.5 mx-auto rounded-[10px] bg-primary-500 font-bold font-sans text-white hover:shadow-[0px_0px_10px_rgba(0,0,0,0.5)] hover:cursor-pointer"
+                className="w-[20%] rounded-[10px] bg-primary-500 font-bold font-sans text-white lg:text-[20px] sm:text-[15px] text-[14px] hover:shadow-[0px_0px_10px_rgba(0,0,0,0.5)] hover:cursor-pointer"
                 disabled={isSubmited}
                 onClick={() => {
                   handleSubmit()
