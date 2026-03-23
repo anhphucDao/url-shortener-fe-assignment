edf99f3df3ef9938ddce95d76f55c1fae2a1565e
Khoi Hoang <khoihoang862006@gmail.com>
2026-03-23 17:53:20 +0700

    Update Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,19 +1,22 @@
 export default function Header() {
   return (
     <>
-      <div className="fixed grid grid-cols-2 w-full h-[11vh] bg-white items-stretch">
+      <div className="fixed grid grid-cols-2 w-full lg:h-[11vh] sm:h-[8vh] h-[5vh] bg-white items-stretch">
         <a
-          className="h-[11vh] aspect-square hover:cursor-pointer"
+          className="lg:h-[11vh] sm:h-[8vh] h-[5vh] aspect-square hover:cursor-pointer"
           href="https://fessior.com/dev-camp/2026"
           target="_blank"
         >
-          <img className="h-[11vh] ml-3" src="/Logo_DemCamp2026.png" alt="demo" />
+          <img className="h-full ml-3" src="/Logo_DemCamp2026.png" alt="demo" />
         </a>
-        <div className="flex h-13 w-50 border border-primary-500 justify-self-end mr-4 my-2 rounded-full">
-          <img className="h-13 w-13 rounded-fuil self-center" src="/default-avatar.png" />
+        <div className="flex lg:h-[11vh] sm:h-[8vh] h-[5vh] w-[50%] border border-primary-500 justify-self-end mr-4 rounded-full">
+          <img
+            className="h-full aspect-ratio-square rounded-fuil self-center"
+            src="/default-avatar.png"
+          />
           <div className="text-[rgb(91,91,91)] ml-4">
-            <div className="text-[12px]">Personal</div>
-            <div className="text-[20px]">Username</div>
+            <div className="lg:text-[15px] sm:text-[14px] text-[8px]">Personal</div>
+            <div className="lg:text-[25px] sm:text-[20px] text-[12px]">Username</div>
           </div>
         </div>
       </div>
