ac38ee152b9b1aa4bdad4b9335d60ea3cf2e1f52
Leonard <duy26332006@gmail.com>
2026-03-24 19:55:28 +0700

    refactor: extract hero section to a component

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,14 @@ import UrlModal from './components/url-modal'
 import ErrorPopup from './components/error-popup'
 import ShortenLinkPopup from './components/shorten-link-popup'
 import useShortenUrl from './utils/useShortenUrl'
+import HeroSection from './components/hero-section'
 function App() {
   const { isLoading, resetStates, handleShorten, setInputUrl, inputUrl, urlState, setUrlState } =
     useShortenUrl()
-
   return (
     <main className="min-h-screen flex items-center justify-center flex-col text-center">
       <Header />
-      <h1 className="-mt-20 w-[80%] md:w-full md:-mt-24 text-4xl md:text-6xl font-bold text-primary-500">
-        Devcamp URL Shortener
-      </h1>
-      <h2 className="w-[80%] md:w-full text-lg md:text-[28px] mt-5 text-primary-500">
-        Simplify, Organize, and Share: URL Management Made Easy
-      </h2>
+      <HeroSection />
       <UrlModal
         onShorten={handleShorten}
         setInputUrl={setInputUrl}
---
+++ src/components/hero-section.tsx
@@ -0,0 +1,13 @@
+function HeroSection() {
+  return (
+    <>
+      <h1 className="-mt-20 w-[80%] md:w-full md:-mt-24 text-4xl md:text-6xl font-bold text-primary-500">
+        Devcamp URL Shortener
+      </h1>
+      <h2 className="w-[80%] md:w-full text-lg md:text-[28px] mt-5 text-primary-500">
+        Simplify, Organize, and Share: URL Management Made Easy
+      </h2>
+    </>
+  )
+}
+export default HeroSection
