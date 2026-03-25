e1a0e54159e4d2b4cb2069a249b2c7077f94a748
khang.le2k72504 <khang.le2k72504@hcmut.edu.vn>
2026-03-25 17:38:56 +0700

    your message

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="text-black p-4 fixed bg-primary-500 flex items-center h-16 w-full">
+    <header className="header text-black p-4 fixed bg-primary-500 flex items-center h-16 w-full">
       <a href="#">
         <img src="PNG\logo_GDGOC.svg" alt="LOGO" className="h-10 w-10 mr-3 cursor-pointer" />
       </a>
