9514aedc7c860338ffdb65dee20d349107b32f16
Nguoigocua <tamhuynh2506@gmail.com>
2026-03-25 09:13:07 +0700

    refactor(image):change .png file's names to lowercase

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -15,17 +15,17 @@ function Popup({ show, onClose }: { show: boolean; onClose: () => void }) {
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="absolute inset-0 bg-black/40 " onClick={onClose}></div>
       <div className="flex flex-col relative items-start  z-10 bg-white w-100 h-120 rounded-lg overflow-hidden ">
-        <img src="/BGPopup.png" alt="BG" className="absolute top-0 left-0  " />
-        <img src="/QRleft.png" className="absolute left-0 z-10 w-13 h-20 mt-8" />
-        <img src="/QRright.png" className="absolute right-0 z-10 w-13 h-20 mt-16" />
+        <img src="/bgPopup.png" alt="BG" className="absolute top-0 left-0  " />
+        <img src="/qrleft.png" className="absolute left-0 z-10 w-13 h-20 mt-8" />
+        <img src="/qrright.png" className="absolute right-0 z-10 w-13 h-20 mt-16" />
         <div className="bg-white w-55 h-55 rounded-lg z-30 absolute top-0 left-0 mt-10 ml-22.5 shadow-lg ">
-          <img src="/QRtest.png" alt="QR code" className="w-50 h-50 mx-2 my-2" />
+          <img src="/qrtest.png" alt="QR code" className="w-50 h-50 mx-2 my-2" />
           <button
             onClick={handleDownload}
             className="absolute -bottom-4 -right-4 flex items-center justify-center w-12 h-12 bg-primary-500 
                     text-white rounded-full  shadow-lg hover:bg-blue-900 transition-colors hover:cursor-pointer"
           >
-            <img src="/Downloadbutton.png" alt="Share" className="w-6 h-6" />
+            <img src="/downloadbutton.png" alt="Share" className="w-6 h-6" />
           </button>
         </div>
         <button
@@ -33,7 +33,7 @@ function Popup({ show, onClose }: { show: boolean; onClose: () => void }) {
           className="absolute top-4 right-4 z-40 flex items-center justify-center 
                 w-8 h-8 bg-gray-100 rounded-full  hover:bg-gray-200 transition-colors hover:cursor-pointer"
         >
-          <img src="/Xbutton.png" alt="X" className="w-3 h-3" />
+          <img src="/xbutton.png" alt="X" className="w-3 h-3" />
         </button>
 
         <div className="absolute bottom-15 w-full text-center px-8 z-20">
