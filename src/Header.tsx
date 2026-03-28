12da521c47b7c846a81e31b17020b13837c5702b
khang.le2k72504 <khang.le2k72504@hcmut.edu.vn>
2026-03-28 22:35:02 +0700

    your message

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="header text-black p-4 fixed bg-primary-500 flex items-center h-16 w-full">
       <a href="#">
-        <img src="PNG\logo_GDGOC.svg" alt="LOGO" className="h-10 w-10 mr-3 cursor-pointer" />
+        <img src="PNG\logo.png" alt="LOGO" className="mr-3 cursor-pointer" />
       </a>
       <h1 className="text-2xl font-bold text-white">Fessior DevCamp 2026</h1>
     </header>
--- src/QR.tsx
+++ src/QR.tsx
@@ -32,18 +32,15 @@ function QR({
         <div className="w-150 h-60 bg-primary-500 absolute bottom-55 transform -rotate-5"></div>
         <span
           className="w-7 h-7 bg-white absolute top-0 right-0 m-3 p-3 rounded-full text-center flex items-center justify-center cursor-pointer 
-                        hover:bg-primary-300 hover:text-white transition-all ease-in delay-.3"
+                        hover:bg-primary-100 hover:text-white transition-all ease-in delay-.3"
+          onClick={onClose}
         >
-          <span className="material-icons scale-80" onClick={onClose}>
-            close
-          </span>
+          <img src="PNG\close.png" alt="close" className="scale-500" />
         </span>
         <div className="relative w-45 h-45 bg-gray-300 border-2 border-white flex items-center justify-center mb-4 border-10 border-gray-500 rounded-xl z-100">
           <span className="text-gray-500">QR Code</span>
-          <span className="flex align-center justify-center bg-primary-500 text-white p-1 rounded-full absolute top-35 left-38 cursor-pointer hover:scale-120 transition-all ease-in delay-.3">
-            <span className="material-icons flex align-center justify-center scale-80">
-              download
-            </span>
+          <span className="w-9 h-9 flex align-center justify-center bg-primary-500 text-white p-1 rounded-full absolute top-38 left-38 cursor-pointer hover:scale-120 transition-all ease-in delay-.3">
+            <img src="PNG\vertical_align_bottom.png" alt="download" />
           </span>
         </div>
         <h2 className="text-xl font-bold text-gray-800 mb-2 z-100">Link shortened!</h2>
@@ -54,12 +51,12 @@ function QR({
           <span className="border-primary-500 border-1 flex items-center flex-1 pl-2 rounded-md">
             {shortenUrl}
           </span>
-          <span
+          <img
+            src="PNG\content_copy.png"
+            alt="copy"
             className="material-icons bg-primary-500 text-white p-2 rounded-md cursor-pointer hover:bg-primary-300"
             onClick={handleCopy}
-          >
-            content_copy
-          </span>
+          ></img>
         </div>
       </div>
     </div>
