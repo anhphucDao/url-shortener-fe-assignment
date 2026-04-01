b886fa76da4899e8c183612548e5dbc8ec018189
Irvine <kirahideyoshi874@gmail.com>
2026-04-01 10:23:07 +0700

    Nguyen Thi Ngoc Quynh
    quynhngoc4076@gmail.com
    
    Building the template UI without updating the Backend.
    
    - Updating file to create the template UI.
    modified:   src/App.css
    modified:   src/App.tsx
    
    - Add new file to assets folder
    new file:   src/assets/IconLink.svg
    new file:   src/assets/Path.svg
    new file:   src/assets/UserProfile.svg
    new file:   src/assets/devcamp_logo_navy.svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+import './App.css'
+import logoImg from './assets/devcamp_logo_navy.svg'
+import userIcon from './assets/UserProfile.svg'
+import Wrapper from './assets/Path.svg'
+
 function App() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-primary-500">URL Shortener</h1>
-    </main>
+    <div className="root">
+      <header className="header">
+        <div className="logo-container">
+          <img src={logoImg} alt="Logo" className="logo-image" />
+        </div>
+
+        <div className="user-profile">
+          <div className="user-avatar">
+            <img src={userIcon} alt="User Avatar" />
+          </div>
+          <div className="user-info">
+            <span className="user-label">Personal</span>
+            <span className="user-name">Username</span>
+          </div>
+        </div>
+      </header>
+
+      <main className="main-content">
+        <h1 className="title">Devcamp URL Shortener</h1>
+        <p className="subtitle">Simplify, Organize, and Share: URL Management Made Easy</p>
+
+        <div className="shorten-card">
+          <label className="input-label">Your long URL</label>
+          <div className="input-group">
+            <div className="input-wrapper">
+              <img src={Wrapper} alt="icon background" className="icon-link" />
+
+              <input
+                type="text"
+                placeholder="Input the URL you want to shortenq"
+                className="url-input"
+              />
+            </div>
+
+            <button className="btn-shorten">Shorten</button>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
