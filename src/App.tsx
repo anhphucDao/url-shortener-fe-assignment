5944dadd11daee86e5c982bc86d9b1111a717773
kuuonkuuon <phamnguyenthienan8.1@gmail.com>
2026-04-07 19:19:28 +0700

    no ai fe
    sửa lại dựa trên web shorten url của fessior, ko ai

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,120 @@
-import { Logo, UserIcon } from './components/Icons'
-import Hero from './components/Hero'
+import { Logo, UserIcon, LinkIcon, DownloadIcon, CopyIcon, CloseIcon } from './components/Icons'
 import { useState } from 'react'
-import Result from './components/Result'
+import QrCode from './assets/qr.png'
+//import Result from './components/Result'
 
 function App() {
-  const [showResult, setShowResult] = useState(false); 
-  const [shortenedUrl/*, setShortenedUrl*/] = useState('https://furl.one/myshortenlink');
+  const [url, setUrl] = useState('')
+  const [showQR, setShowQR] = useState(false)
+
+  const handleShorten = () => {
+    if (url.trim()) {
+      setShowQR(true)
+      console.log('https://furl.one/myshortenlink')
+    }
+  }
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="w-full bg-white flex justify-between items-center px-10 py-[17px]">
-        
-        <div className="flex-shrink-0">
-          <Logo className="w-[96px] h-[74px]" />
-        </div>
-
-        <div className="flex items-center border border-primary-500 rounded-full p-0 pr-6 gap-3">
-          <UserIcon className="w-[52px] h-[52px] flex-shrink-0" />
-          <div className="flex flex-col">
-            <span className="text-xs text-neutral-500">Personal</span>
-            <span className="text-sm font-medium text-neutral-800 -mt-0.5">Username</span>
+    <div className="flex min-h-screen flex-col">
+      <div
+        id="header"
+        className="fixed z-10 w-full flex justify-between items-center p-5 md:py-3.5 lg:px-7 lg:py-5 bg-white"
+      >
+        <Logo className="" />
+        <div className="border border-primary flex w-full items-center rounded-full lg:w-[170px] me-3 ms-5">
+          <div className="flex w-full items-center pe-3">
+            <UserIcon className="" />
+            <div className="flex flex-col">
+              <span className="text-[12px] font-normal text-[#5B5B5B] font-sans">Personal</span>
+              <span className="text-[20px] font-medium text-[#5B5B5B] font-sans">Username</span>
+            </div>
           </div>
         </div>
-
-      </header>
-
-      <main className="flex-1 flex items-center justify-center">
-        <Hero onShorten={() => setShowResult(true)} />
+      </div>
+      <main className="flex-1 flex flex-col items-center justify-center gap-[10px]">
+        <span className="text-[60px] font-bold text-primary-500 font-sans">
+          Devcamp URL Shortener
+        </span>
+        <span className="text-[28px] font-normal text-primary-500 font-sans">
+          Simplify, Organize, and Share: URL Management Made Easy
+        </span>
+        <div className="items-start w-full max-w-[800px] mx-auto h-[119px] bg-white border border-[#7E7E7E]/30 rounded-lg shadow-[0px_4px_47px_0_rgba(11,40,120,0.3)] p-6">
+          <div className="items-start md:flex">
+            <div className="md:grow">
+              <div>
+                <span className="text-[20px] font-semibold text-primary-500 font-sans">
+                  Your long URL
+                </span>
+                <div className="flex flex-row max-w-[608px] h-[42px] bg-white border border-[#7E7E7E]/30 rounded-lg">
+                  <div className="flex items-center gap-2 px-[8px] py-[11px] w-full h-11">
+                    <LinkIcon className="" />
+                    <input
+                      value={url}
+                      onChange={e => setUrl(e.target.value)}
+                      className="flex-1 w-full text-[12px] font-normal text-primary-300 font-sans outline-none h-full grow"
+                      placeholder="Input the URL you want to shorten"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button
+              disabled={!url}
+              onClick={handleShorten}
+              className={
+                !url
+                  ? 'w-fit bg-[#9da9c9] text-[#0b2878] cursor-not-allowed opacity-75 text-base px-3 py-2 rounded-lg flex min-w-fit items-center justify-center duration-100 md:relative md:top-8'
+                  : 'w-fit bg-[#0b2878] text-white text-base px-3 py-2 rounded-lg flex min-w-fit items-center justify-center duration-100 md:relative md:top-8'
+              }
+            >
+              Shorten
+            </button>
+          </div>
+        </div>
       </main>
-
-      {showResult && (
-        <Result 
-          shortUrl={shortenedUrl} 
-          onClose={() => setShowResult(false)} 
-        />
+      {showQR && (
+        <div className="fixed inset-0 z-10 flex items-center justify-center bg-black/30">
+          <div className="relative mx-3 flex aspect-[9/10] w-[400px] rounded-lg bg-primary-500 overflow-hidden">
+            <div className="absolute inset-0 z-10 flex flex-col items-center justify-between px-5 py-10 md:p-10">
+              <div className="relative flex aspect-square h-1/2 items-center justify-center rounded-lg bg-white p-3 shadow-[0px_4px_12px_0px_rgba(11,40,120,0.16)]">
+                <img src={QrCode} alt="QR Code" className="size-full" />
+                <a
+                  download="qr-code.png"
+                  href={QrCode}
+                  className="absolute -bottom-3 -right-3 aspect-square w-9 rounded-full bg-primary p-1 hover:cursor-pointer"
+                >
+                  <DownloadIcon className="size-auto" />
+                </a>
+              </div>
+              <div className="mb-3 flex grow flex-col items-center justify-center px-3">
+                <span className="mb-2 text-2xl font-bold text-primary-500">Link shortened!</span>
+                <span className="text-center text-primary-500">
+                  Access the “My URL” page to view statistics on your shortened links
+                </span>
+              </div>
+              <div className="flex h-10 w-full content-between">
+                <div className="me-1 flex grow items-center overflow-hidden rounded-lg border border-solid border-primary p-2">
+                  <span className="truncate text-primary-500">https://furl.one/myshortenlink</span>
+                </div>
+                <button
+                  title="Copy Button"
+                  className="w-fit border-[#0B2878] text-[#0B2878] hover:bg-[#bcccf9] border h-10 text-base aspect-square p-2 rounded-lg relative flex min-w-fit items-center justify-center overflow-hidden transition-all duration-100"
+                >
+                  <CopyIcon className="h-full w-auto" />
+                </button>
+              </div>
+            </div>
+            <div className="absolute bottom-[-64px] left-[-30px] h-4/5 w-[150%] -rotate-6 bg-white"></div>
+            <button
+              onClick={() => setShowQR(false)}
+              title="Close Button"
+              className="absolute right-3 top-3 z-10 flex aspect-square w-9 items-center justify-center rounded-full bg-white p-1 hover:cursor-pointer md:right-4 md:top-4"
+            >
+              <CloseIcon className="size-auto" />
+            </button>
+          </div>
+        </div>
       )}
-
     </div>
   )
 }
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,57 +1,91 @@
-import logo from '../assets/logo.png'
+import logo from '../assets/devcamp_logo_navy.svg'
 
-interface IconProps {
-  className?: string;
+interface IconProperties {
+  className?: string
 }
 
-export const UserIcon = ({ className }: IconProps) => (
-  <svg 
-    viewBox="0 0 52 52" 
-    fill="none" 
-    xmlns="http://www.w3.org/2000/svg"
+export const UserIcon = ({ className }: IconProperties) => (
+  <svg
     className={className}
+    width="52"
+    height="52"
+    viewBox="0 0 52 52"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M26 0C11.648 0 0 11.648 0 26C0 40.352 11.648 52 26 52C40.352 52 52 40.352 52 26C52 11.648 40.352 0 26 0ZM26 7.8C30.316 7.8 33.8 11.284 33.8 15.6C33.8 19.916 30.316 23.4 26 23.4C21.684 23.4 18.2 19.916 18.2 15.6C18.2 11.284 21.684 7.8 26 7.8ZM26 44.72C19.5 44.72 13.754 41.392 10.4 36.348C10.478 31.174 20.8 28.34 26 28.34C31.174 28.34 41.522 31.174 41.6 36.348C38.246 41.392 32.5 44.72 26 44.72Z" 
-    fill="#0B2878"
+    <path
+      d="M26 0C11.648 0 0 11.648 0 26C0 40.352 11.648 52 26 52C40.352 52 52 40.352 52 26C52 11.648 40.352 0 26 0ZM26 7.8C30.316 7.8 33.8 11.284 33.8 15.6C33.8 19.916 30.316 23.4 26 23.4C21.684 23.4 18.2 19.916 18.2 15.6C18.2 11.284 21.684 7.8 26 7.8ZM26 44.72C19.5 44.72 13.754 41.392 10.4 36.348C10.478 31.174 20.8 28.34 26 28.34C31.174 28.34 41.522 31.174 41.6 36.348C38.246 41.392 32.5 44.72 26 44.72Z"
+      fill="#0B2878"
     />
   </svg>
-);
+)
 
-export const Logo = ({ className }: IconProps) => (
-  <svg 
-    viewBox="0 0 96 74" 
-    fill="none" 
-    xmlns="http://www.w3.org/2000/svg" 
-    xmlnsXlink="http://www.w3.org/1999/xlink"
+export const Logo = ({ className }: IconProperties) => (
+  <img src={logo} alt="Logo" className={className} />
+)
+
+export const LinkIcon = ({ className }: IconProperties) => (
+  <svg
     className={className}
+    width="17"
+    height="9"
+    viewBox="0 0 17 9"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
   >
-    <rect width="96" height="74" fill="url(#pattern0_1_99)"/>
-    <defs>
-      <pattern id="pattern0_1_99" patternContentUnits="objectBoundingBox" width="1" height="1">
-        <use 
-            xlinkHref="#image0_1_99" 
-            transform="matrix(0.00113525 0 0 0.00147275 -0.00064433 0)"
-        />
-      </pattern>
-      <image 
-        id="image0_1_99" 
-        width="882" 
-        height="679" 
-        preserveAspectRatio="none" 
-        href={logo} 
-      />
-    </defs>
+    <path
+      d="M1.58333 4.16667C1.58333 2.74167 2.74167 1.58333 4.16667 1.58333H7.5V0H4.16667C1.86667 0 0 1.86667 0 4.16667C0 6.46667 1.86667 8.33333 4.16667 8.33333H7.5V6.75H4.16667C2.74167 6.75 1.58333 5.59167 1.58333 4.16667ZM5 5H11.6667V3.33333H5V5ZM12.5 0H9.16667V1.58333H12.5C13.925 1.58333 15.0833 2.74167 15.0833 4.16667C15.0833 5.59167 13.925 6.75 12.5 6.75H9.16667V8.33333H12.5C14.8 8.33333 16.6667 6.46667 16.6667 4.16667C16.6667 1.86667 14.8 0 12.5 0Z"
+      fill="#0B2878"
+      fill-opacity="0.87"
+    />
   </svg>
-);
+)
 
-export const CloseIcon = ({ className }: IconProps) => (
-  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+export const CloseIcon = ({ className }: IconProperties) => (
+  <svg
+    className={className}
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M21.8333 11.3417L20.6583 10.1667L16 14.825L11.3417 10.1667L10.1667 11.3417L14.825 16L10.1667 20.6584L11.3417 21.8334L16 17.175L20.6583 21.8334L21.8333 20.6584L17.175 16L21.8333 11.3417Z"
+      fill="#333333"
+    />
   </svg>
-);
+)
 
-export const CopyIcon = ({ className }: IconProps) => (
-  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+export const CopyIcon = ({ className }: IconProperties) => (
+  <svg
+    className={className}
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M24 9H12C10.9 9 10 9.9 10 11V25H12V11H24V9ZM27 13H16C14.9 13 14 13.9 14 15V29C14 30.1 14.9 31 16 31H27C28.1 31 29 30.1 29 29V15C29 13.9 28.1 13 27 13ZM27 29H16V15H27V29Z"
+      fill="#0b2878"
+    />
   </svg>
-);
+)
+
+export const DownloadIcon = ({ className }: IconProperties) => (
+  <svg
+    className={className}
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="20" cy="20" r="20" fill="#0B2878" />
+    <path
+      d="M24.6667 21.1667H21.1667V9.5H18.8333V21.1667H15.3333L20 25.8333L24.6667 21.1667ZM10.6667 28.1667V30.5H29.3333V28.1667H10.6667Z"
+      fill="white"
+    />
+  </svg>
+)
