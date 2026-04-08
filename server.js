e8118c1e0ce73fb3bb99f68d865b56a478323e14
phuong-tran2006 <phuong.tran1284@hcmut.edu.vn>
2026-04-09 01:34:04 +0700

    Cập nhật lỗi thiếu thư viện và chỉnh sửa giao diện

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 const db = {}
 
 app.use(cors({ origin: '*' }))
