b6a1d64906305fabc485f5c052ab98cda618cef2
TQD1911 <dotrungquan.1911@gmail.com>
2026-04-09 22:38:10 +0700

    change directory

--- src/components/Result/DownloadButton.tsx
+++ src/components/Result/DownloadButton.tsx
@@ -6,7 +6,7 @@ function DownloadButton() {
       onClick={() => alert('Downloaded!')}
       className="w-[40px] h-[40px] absolute top-[200px] left-[200px] bg-primary-500 rounded-full"
     >
-      <img src="/images/download_icon.svg" className="w-[28px] h-[28px]" />
+      <img src="images/download_icon.svg" className="w-[28px] h-[28px]" />
     </Button>
   )
 }
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div className="w-[199px] h-[52px] items-center flex border border-primary-500 border-l-0 rounded-full gap-[12px]">
-      <img src="/images/user.svg" className="w-[52px] h-[52px]" />
+      <img src="images/user.svg" className="w-[52px] h-[52px]" />
       <div className="w-[199px] h-[52px] py-[4px] justify-center flex flex-col">
         <h3 className="text-xs text-gray-500">Personal</h3>
         <h2 className="text-xl text-gray-500 font-semibold">Username</h2>
