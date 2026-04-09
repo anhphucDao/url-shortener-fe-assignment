1b13cfe3c4902dd5044797235ed2efdd9501f78b
phuong-tran2006 <phuong.tran1284@hcmut.edu.vn>
2026-04-09 21:00:42 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
     setIsLoading(true)
     try {
-      const response = await fetch('http://localhost:10000/api/shorten', {
+      const response = await fetch('https://url-shortener-fe-assignment-1.onrender.com/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url: urlInput }),
